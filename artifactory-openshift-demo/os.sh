f161f67d40ccb7f4b8cf5fb2390a1199cd323e30
Shikhar Rawat <lpshikhar@live.in>
2017-04-27 12:49:56 -0700

    made version specific

--- artifactory-openshift-demo/os.sh
+++ artifactory-openshift-demo/os.sh
@@ -2,7 +2,7 @@
 ## Maintained by Shikhar Rawat, JFrog Inc. ##
 #######
 #! /bin/bash
-oc cluster up --version=latest
+oc cluster up --version=v3.4
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin developer
 oc adm policy add-scc-to-user anyuid developer
